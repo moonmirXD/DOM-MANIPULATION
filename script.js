e78a270eececb22c893a072f46c7b294baa98555
Ryan Arqueza <arquezaryan@gmail.com>
2022-02-23 11:23:15 +0800

    Implementing highscore

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 'use strict';
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -18,6 +19,11 @@ document.querySelector('.check').addEventListener('click', function () {
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
   }
 
   // When guess is high
